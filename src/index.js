4f4d23521115f90f3cbef397c71ee2a2530d5edb
Anderson Leonardo <andersonleonardo.sapinto@gmail.com>
2023-01-08 12:20:32 -0300

    feat: index e connection

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import router from './router/indexRouter.js'
+
+dotenv.config();
+
+const server = express()
+server.use(cors);
+server.use(express.json());
+server.use(router);
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}...`);
+});
